41e423edb28035b38e0f46c57f07aa92912ac792
sujayneglur123@gmail.com <sujayneglur123@gmail.com>
2019-06-06 21:38:34 -0400

    Data stream median

--- src/Q763/LabelPartition.java
+++ src/Q763/LabelPartition.java
@@ -1,10 +1,11 @@
 package Q763;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+
 class Index{
     int start;
     int end;
+    boolean isDescending = false;
 
     public Index(int start, int end){
         this.start = start;
@@ -12,8 +13,15 @@ class Index{
     }
 }
 public class LabelPartition {
+
+    public void print(LinkedHashMap<Character,Index> map){
+        for(Map.Entry<Character,Index> entry:map.entrySet()){
+            System.out.println(entry.getKey()+"---"+entry.getValue().start+" , "+entry.getValue().end);
+        }
+    }
+
     public List<Integer> partitionLabels(String s){
-        HashMap<Character,Index> charIndex = new HashMap<>();
+        LinkedHashMap<Character,Index> charIndex = new LinkedHashMap<>();
         for(int i=0;i<s.length();i++){
             if(!charIndex.containsKey(s.charAt(i))){
                 charIndex.put(s.charAt(i),new Index(i,i));
@@ -23,12 +31,36 @@ public class LabelPartition {
                 charIndex.put(s.charAt(i),new Index(index.start,i));
             }
         }
-        System.out.println(charIndex.values());
-        return null;
+        print(charIndex);
+        List<Integer> result = new ArrayList<>();
+        Stack<Index> stack = new Stack<>();
+        Index [] endIndices = charIndex.values().toArray(new Index[charIndex.size()]);
+        int start = endIndices[0].start;
+        int end = endIndices[0].end;
+//        result.add(end-start+1);
+        boolean isDescending=false;
+        for(int i=1;i<endIndices.length;i++){
+            Index index = endIndices[i];
+            if(index.start>=start && index.end>=end){
+                if(index.start>=end){
+                    result.add(end-start+1);
+                    start = index.start;
+                    end = index.end;
+                }
+                else{
+                    end = index.end;
+                }
+            }
+            else{
+                System.out.println("Got here for start "+index.start+" and end "+index.end);
+            }
+        }
+        result.add(end-start+1);
+        return result;
     }
 
     public static void main(String[] args) {
         LabelPartition lp = new LabelPartition();
-        lp.partitionLabels("abcdabcd");
+        System.out.println(lp.partitionLabels("abcammde"));
     }
 }
